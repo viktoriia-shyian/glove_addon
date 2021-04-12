2207730f3df7f0b77412d180e85a9c713e4a9028
VYanhy <viktoriya.shyyan@gmail.com>
2021-04-13 02:41:10 +0300

    little edit

--- arduino_sketch (arduino ide)/sketch_nano_glove/sketch_nano_glove.ino
+++ arduino_sketch (arduino ide)/sketch_nano_glove/sketch_nano_glove.ino
@@ -20,15 +20,13 @@ void setup()
 
   Wire.begin();
   Serial.begin(38400);
+
   mpu1.initialize();
   mpu2.initialize();
   mpu3.initialize();
   mpu4.initialize();
   mpu5.initialize();
   mpu6.initialize();
-
-  mpu1.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
-  mpu1.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
 }
 
 void loop()
@@ -38,7 +36,7 @@ void loop()
 
     switch ()
     {
-    //check connection
+    // connection state
     case 0:
     {
       Serial.println(mpu1.testConnection() ? "MPU6050 1 OK" : "MPU6050 1 FAIL");
@@ -48,98 +46,80 @@ void loop()
       Serial.println(mpu5.testConnection() ? "MPU6050 5 OK" : "MPU6050 5 FAIL");
       Serial.println(mpu6.testConnection() ? "MPU6050 6 OK" : "MPU6050 6 FAIL");
     }
-    //calibration
+    // sensitivity setting
     case 1:
+    {
+      set_sensitivity();
+      set_sensitivity(MPU6050_ACCEL_FS_2, MPU6050_GYRO_FS_250);
+    }
+    //calibration
+    case 2:
     {
       calibration();
     }
     //send data to call
-    case 2:
+    case 3:
     {
       send_data();
     }
-    case 3:
+    case 4:
     {
     }
-    }
-
-    //get data
-    send_data();
-    Serial.println();
-    delay(1000);
-
-    if (state == 0)
+    default:
     {
-      Serial.println("\nReading sensors for first time...");
-      meansensors();
-      state++;
+      send_data();
+      Serial.println();
       delay(1000);
     }
-    if (state == 1)
-    {
-      Serial.println("\nCalculating offsets...");
-      calibration();
-      state++;
-      delay(1000);
-    }
-    if (state == 2)
-    {
-      meansensors();
-      Serial.println("\nFINISHED!");
-      Serial.print("\nSensor readings with offsets:\t");
-      Serial.print(mean_ax);
-      Serial.print("\t");
-      Serial.print(mean_ay);
-      Serial.print("\t");
-      Serial.print(mean_az);
-      Serial.print("\t");
-      Serial.print(mean_gx);
-      Serial.print("\t");
-      Serial.print(mean_gy);
-      Serial.print("\t");
-      Serial.println(mean_gz);
-      Serial.print("Your offsets:\t");
-      Serial.print(ax_offset);
-      Serial.print(", ");
-      Serial.print(ay_offset);
-      Serial.print(", ");
-      Serial.print(az_offset);
-      Serial.print(", ");
-      Serial.print(gx_offset);
-      Serial.print(", ");
-      Serial.print(gy_offset);
-      Serial.print(", ");
-      Serial.println(gz_offset);
-      Serial.println("\nData is printed as: acelX acelY acelZ giroX giroY giroZ");
-      Serial.println("Check that your sensor readings are close to 0 0 16384 0 0 0");
-      Serial.println("If calibration was succesful write down your offsets so you can set them in your projects using something similar to mpu.setXAccelOffset(youroffset)");
-      while (1)
-        ;
     }
   }
 }
 
+void set_sensitivity()
+{
+  mpu1.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
+  mpu1.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
+  mpu2.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
+  mpu2.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
+  mpu3.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
+  mpu3.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
+  mpu4.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
+  mpu4.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
+  mpu5.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
+  mpu5.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
+  mpu6.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
+  mpu6.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
+}
+
+void set_sensitivity(uint8_t accel, uint8_t gyro)
+{
+  mpu1.setFullScaleAccelRange(accel);
+  mpu1.setFullScaleGyroRange(gyro);
+}
+
 void send_data()
 {
-
   mpu1.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  send_data();
+  serial_send();
 
   mpu2.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  send_data();
+  serial_send();
 
   mpu3.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  send_data();
+  serial_send();
 
   mpu4.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  send_data();
+  serial_send();
 
   mpu5.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  send_data();
+  serial_send();
 
   mpu6.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  send_data();
+  serial_send();
+}
 
+void serial_send()
+{
   Serial.print(ax);
   Serial.print('\t');
   Serial.print(ay);
@@ -153,76 +133,6 @@ void send_data()
   Serial.println(gz);
 }
 
-void meansensors()
-{
-  long i = 0, buff_ax = 0, buff_ay = 0, buff_az = 0, buff_gx = 0, buff_gy = 0, buff_gz = 0;
-  while (i < (buffersize + 101))
-  { // read raw accel/gyro measurements from device mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz); if (i > 100 && i <= (buffersize + 100)) { //First 100 measures are discarded
-    buff_ax = buff_ax + ax;
-    buff_ay = buff_ay + ay;
-    buff_az = buff_az + az;
-    buff_gx = buff_gx + gx;
-    buff_gy = buff_gy + gy;
-    buff_gz = buff_gz + gz;
-  }
-  if (i == (buffersize + 100))
-  {
-    mean_ax = buff_ax / buffersize;
-    mean_ay = buff_ay / buffersize;
-    mean_az = buff_az / buffersize;
-    mean_gx = buff_gx / buffersize;
-    mean_gy = buff_gy / buffersize;
-    mean_gz = buff_gz / buffersize;
-  }
-  i++;
-  delay(2);
-}
-}
-
 void calibration()
 {
-  ax_offset = -mean_ax / 8;
-  ay_offset = -mean_ay / 8;
-  az_offset = (16384 - mean_az) / 8;
-  gx_offset = -mean_gx / 4;
-  gy_offset = -mean_gy / 4;
-  gz_offset = -mean_gz / 4;
-  while (1)
-  {
-    int ready = 0;
-    mpu.setXAccelOffset(ax_offset);
-    mpu.setYAccelOffset(ay_offset);
-    mpu.setZAccelOffset(az_offset);
-    mpu.setXGyroOffset(gx_offset);
-    mpu.setYGyroOffset(gy_offset);
-    mpu.setZGyroOffset(gz_offset);
-    meansensors();
-    Serial.println("...");
-    if (abs(mean_ax) <= acel_deadzone)
-      ready++;
-    else
-      ax_offset = ax_offset - mean_ax / acel_deadzone;
-    if (abs(mean_ay) <= acel_deadzone)
-      ready++;
-    else
-      ay_offset = ay_offset - mean_ay / acel_deadzone;
-    if (abs(16384 - mean_az) <= acel_deadzone)
-      ready++;
-    else
-      az_offset = az_offset + (16384 - mean_az) / acel_deadzone;
-    if (abs(mean_gx) <= gyro_deadzone)
-      ready++;
-    else
-      gx_offset = gx_offset - mean_gx / (gyro_deadzone + 1);
-    if (abs(mean_gy) <= gyro_deadzone)
-      ready++;
-    else
-      gy_offset = gy_offset - mean_gy / (gyro_deadzone + 1);
-    if (abs(mean_gz) <= gyro_deadzone)
-      ready++;
-    else
-      gz_offset = gz_offset - mean_gz / (gyro_deadzone + 1);
-    if (ready == 6)
-      break;
-  }
 }
